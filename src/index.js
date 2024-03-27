5d6bb2cfc8750aea49c7f4df16b01e3cd523a4ce
Alle024gg <alle024ggnanana@gmail.com>
2024-03-27 21:08:48 +0200

    create mk&seo page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./style.css";
+document
+  .getElementById("categories-list")
+  .addEventListener("click", function (event) {
+    var target = event.target;
+    if (target.tagName === "A") {
+      var href = target.getAttribute("href");
+      window.location.href = href;
+    }
+  });
