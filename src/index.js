58e306a361e0a1db0329007d4f89adeaba631b51
eduard96c <ciubaneduard1196@gmail.com>
2024-03-27 12:57:04 -0700

    restructured code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import "./style.css";
-document
-  .getElementById("categories-list")
-  .addEventListener("click", function (event) {
-    var target = event.target;
-    if (target.tagName === "A") {
-      var href = target.getAttribute("href");
-      window.location.href = href;
-    }
+
+function addEvents() {
+  var continueReadingButtons = document.querySelectorAll(".continue_reading");
+  continueReadingButtons.forEach(function (button) {
+    button.addEventListener("click", _handleContinueReading);
   });
+}
+
+function _handleContinueReading() {
+  var contentsmall = this.nextElementSibling;
+  if (contentsmall.style.display === "block") {
+    contentsmall.style.display = "none";
+  } else {
+    contentsmall.style.display = "block";
+  }
+  console.log("Button clicked. Display style: " + contentsmall.style.display);
+}
+
+addEvents();
