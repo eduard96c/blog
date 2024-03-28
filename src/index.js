5ba9485156f815c2988bdfe0402209ca5ae4924b
eduard96c <ciubaneduard1196@gmail.com>
2024-03-28 09:39:16 -0700

    js comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import "./style.css";
 
+//declarari functii
+
+//setam toate event-urile de pe pagina in functia addEvents()
 function addEvents() {
   var continueReadingButtons = document.querySelectorAll(".continue_reading");
   continueReadingButtons.forEach(function (button) {
+    //la fiecare click se apeleaza functia _handleContinueReading
     button.addEventListener("click", _handleContinueReading);
   });
 }
@@ -17,4 +21,5 @@ function _handleContinueReading() {
   console.log("Button clicked. Display style: " + contentsmall.style.display);
 }
 
+//apelare functii
 addEvents();
