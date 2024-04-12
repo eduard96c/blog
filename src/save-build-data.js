42c2cd4db660e5b716e1c9bb54427862dc8956b7
eduard96c <ciubaneduard1196@gmail.com>
2024-04-12 10:10:06 -0700

    new build

---
+++ src/save-build-data.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const articles_data = "articles-data.json";
+const api_articles_data = "../blog-API/data/articles.json";
+
+function getApiData() {
+  return fs.readFileSync(api_articles_data);
+}
+
+function saveArticles() {
+  fs.writeFileSync("./src/" + articles_data, getApiData());
+
+  fs.copyFile("./src/" + articles_data, "./docs/" + articles_data, (err) => {
+    if (err) throw err;
+    console.log("source.txt was copied to destination.txt");
+  });
+}
+
+function getArticles() {
+  const content = fs.readFileSync("./src/" + articles_data);
+  return JSON.parse(content);
+}
+
+function getImages() {
+  let images = [];
+  const articles = getArticles();
+  articles.forEach((element) => {
+    if (element.image.trim() !== "") {
+      images.push(element.image);
+    }
+  });
+  return images;
+}
+
+function saveImages() {
+  const images = getImages();
+  images.forEach(function (img) {
+    fs.copyFile("./src/images/" + img, "./docs/" + img, (err) => {
+      if (err) throw err;
+      console.log("source.txt was copied to destination.txt");
+    });
+  });
+}
+
+saveArticles();
+saveImages();
